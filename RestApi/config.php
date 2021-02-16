041449c22ac77beffb417f2b09b5c7191230f2f0
diegomeidas <diegomeidas@hotmail.com>
2021-02-15 21:48:25 -0300

    Update config.php

--- RestApi/config.php
+++ RestApi/config.php
@@ -1,8 +1,15 @@
 <?php
 
-    //Mysql
-    const DBDRIVE = 'mysql';
+    //local
+    /* const DBDRIVE = 'mysql';
     const DBHOST = 'localhost';
     const DBNAME = 'meidasapi';
     const DBUSER = 'root';
-    const DBPASS = '';
+    const DBPASS = ''; */
+
+    //server
+    const DBDRIVE = 'mysql';
+    const DBHOST = 'localhost';
+    const DBNAME = 'u765433036_meidasapi';
+    const DBUSER = 'u765433036_ftj';
+    const DBPASS = 'Di@101133';
