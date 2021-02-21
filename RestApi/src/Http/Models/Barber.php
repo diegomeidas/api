af9c89eda01343ead0e94919d5982dc221191705
diegomeidas <diegomeidas@hotmail.com>
2021-02-21 07:39:03 -0300

    Create Barber.php

---
+++ RestApi/src/Http/Models/Barber.php
@@ -0,0 +1,73 @@
+<?php
+    namespace Api\Http\Models;
+
+    class Barber
+    {
+        private static $table = 'tb_barber';
+
+        public static function select(int $id) {
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'SELECT * FROM '.self::$table.' WHERE id = :id';
+            $stmt = $connPdo->prepare($sql);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetch(\PDO::FETCH_ASSOC);
+            } else {
+                throw new \Exception("Nenhum usuário encontrado!");
+            }
+        }
+
+        public static function selectUser($email, $password) {
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'SELECT * FROM '.self::$table.' WHERE email = :email AND password = :password';
+            $stmt = $connPdo->prepare($sql);
+            $stmt->bindValue(':email', $email);
+            $stmt->bindValue(':password', $password);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetch(\PDO::FETCH_ASSOC);
+            } else {
+                throw new \Exception("Nenhum usuário encontrado!");
+            }
+        }
+
+        public static function selectAll() {
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'SELECT * FROM '.self::$table;
+            $stmt = $connPdo->prepare($sql);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            } else {
+                throw new \Exception("Nenhum usuário encontrado!");
+            }
+        }
+
+        public static function insert($data)
+        {
+            var_dump($data);exit;
+
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'INSERT INTO '.self::$table.' (name, email, password) VALUES (:na, :em, :pa)';
+            $stmt = $connPdo->prepare($sql);
+            $stmt->bindValue(':na', $data['name']);
+            $stmt->bindValue(':em', $data['email']);
+            $stmt->bindValue(':pa', $data['password']);
+            
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return 'Usuário(a) inserido com sucesso!';
+            } else {
+                throw new \Exception("Falha ao inserir usuário(a)!");
+            }
+        }
+    }
