ffafa343262ddfe817bed8e238452fdce7e30178
diegomeidas <diegomeidas@hotmail.com>
2021-04-22 20:34:20 -0300

    Update config.php

--- RestApi/config.php
+++ RestApi/config.php
@@ -8,8 +8,15 @@
     const DBPASS = ''; */
 
     //server
-    const DBDRIVE = 'mysql';
+    /* const DBDRIVE = 'mysql';
     const DBHOST = 'localhost';
     const DBNAME = 'u765433036_meidasapi';
     const DBUSER = 'u765433036_u765433036_ftj';
+    const DBPASS = 'Di@101133'; */
+
+    
+    const DBDRIVE = 'mysql';
+    const DBHOST = 'localhost';
+    const DBNAME = 'u765433036_natureapi';
+    const DBUSER = 'u765433036_api';
     const DBPASS = 'Di@101133';
