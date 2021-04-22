0fc26fe261306c227ad3d6c1869ac782c5cbc8cc
diegomeidas <diegomeidas@hotmail.com>
2021-04-22 20:34:11 -0300

    Create ProducerController.php

---
+++ RestApi/src/Http/Controllers/ProducerController.php
@@ -0,0 +1,45 @@
+<?php
+    namespace Api\Http\Controllers;
+    use Api\Http\Models\User;
+    
+    class ProducerController {
+
+        public function getAll() 
+        {
+			
+			if (AuthController::checkToken()) {
+				
+                return Producer::selectAll();
+                
+			}
+			
+			throw new \Exception('Não autenticado');
+            
+        }
+
+        public function get($id = null) 
+        {
+			
+			if (AuthController::checkToken()) {
+				
+				if ($id) {
+					return Barber::select($id);
+				} else {
+					return Barber::selectAll();
+				}
+			}
+			
+			throw new \Exception('Não autenticado');
+            
+        }
+
+        public function post() 
+        {
+            //return $_POST;
+            $data = $_POST;
+
+            return User::insert($data);
+        }
+
+    }
+        
